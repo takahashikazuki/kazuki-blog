40555a90e77baf29647ab1ba8ea1111a95d9a140
Kazuki Takahashi <kazuki.takahashi@newt.so>
2021-07-05 14:20:07 +0900

    fix: title

--- src/components/header.js
+++ src/components/header.js
@@ -24,7 +24,7 @@ const Header = ({ siteTitle }) => (
             textDecoration: `none`,
           }}
         >
-          {siteTitle} XXX
+          {siteTitle}
         </Link>
       </h1>
     </div>
